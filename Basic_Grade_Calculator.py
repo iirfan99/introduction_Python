b3a61adacd05d2dec474ab88ee9a313c13d5e826
iirfan99 <57878709+iirfan99@users.noreply.github.com>
2021-02-11 22:52:42 +0300

    Add files via upload
    introduction to if-else at python

---
+++ Basic_Grade_Calculator.py
@@ -0,0 +1,15 @@
+grade =float(input("please enter your grade "))
+
+if grade >=5:
+     print("you got very amazing {0} grade".format(grade))
+elif grade >=4:
+     print("you pass  very good {0} grade".format(grade))
+
+
+elif grade >=3 :
+    print("you passed the course with {0} grade".format(grade))
+
+
+
+else:
+    print("you failed the course {0} grade".format(grade))
