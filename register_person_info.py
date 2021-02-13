bf67409d8e83ba61d2d2f4f341448390e6902e47
iirfan99 <57878709+iirfan99@users.noreply.github.com>
2021-02-13 03:25:49 +0300

    Add files via upload

---
+++ register_person_info.py
@@ -0,0 +1,8 @@
+def register(name="no info",surname="no info",age="no info ",job="no info"):
+    print("adding record ")
+
+    print("Name :{}\nsurname:{}\nage:{}\njob:{}\n".format(name,surname,age,job))
+    print("it is added correctly")
+
+
+register(name="Kamil",surname="kaplan",age=21,job="Doctor")
